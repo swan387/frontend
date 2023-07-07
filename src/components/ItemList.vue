e6597498ea353b0f492f3525ef45ada781011df3
nani432 <32137742+xBibbb@users.noreply.github.com>
2023-07-07 18:11:58 +1200

    update font, color and pdf preview page

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -3,7 +3,10 @@
         <!-- title, welcome message -->
         <div class="header">
             <h1 class="page-header">Welcome Back, <br>There are {{ item_list.length }} Production Lines.</h1>
-            <img src="@/assets/logo.png" alt="">
+            <el-tooltip placement="bottom">
+                <template #content><span>Click to Refresh</span></template>
+                <img src="@/assets/logo.png" alt="">
+            </el-tooltip>
         </div>
 
         <!-- search bar -->
@@ -22,7 +25,7 @@
         <!-- show description for each production line -->
         <template v-for="(item, index) of filteredList" :key="item.PO">
             <!-- :title="'Production Line ' + (index + 1)" -->
-            <el-descriptions :column="2" size="small" border class="desc-item">
+            <el-descriptions :column="2" size="default" border class="desc-item">
                 <el-descriptions-item label="Prod. Line" label-align="left" align="left">
                     {{ index + 1 }} <el-tag size="small">{{ item.Status }}</el-tag>
                 </el-descriptions-item>
@@ -44,9 +47,9 @@
                 <el-descriptions-item label="PDF Files" label-align="left" align="left">
                     <!-- <div class="pdf-buttons"> -->
                     <el-button-group>
-                        <el-button type="" @click="viewPDF" plain size="small">PO</el-button>
-                        <el-button type="" @click="viewPDF" plain size="small">SKU1</el-button>
-                        <el-button type="" @click="viewPDF" plain size="small">SKU2</el-button>
+                        <el-button type="" @click="viewPDF" plain size="default">PO</el-button>
+                        <el-button type="" @click="viewPDF" plain size="default">SKU1</el-button>
+                        <el-button type="" @click="viewPDF" plain size="default">SKU2</el-button>
                     </el-button-group>
                     <!-- <el-button type="primary" plain small>Open File</el-button> -->
                     <!-- </div> -->
@@ -54,15 +57,23 @@
             </el-descriptions>
         </template>
 
-        <el-drawer v-model="showPDF" title="I am the title" :with-header="false" size="80%">
+        <el-drawer v-model="showPDF" :with-header="false" size="100%" :close-on-click-modal="false" :show-close="false">
             <!-- append-to-body -->
             <!-- el-drawer__body -->
-            <embed src="https://arxiv.org/pdf/2306.17459.pdf" type="application/pdf" width="100%" height="99.7%">
+            <div class="pdf-bottom-container">
+                <div style="flex-grow: 1;">
+                    <embed src="https://arxiv.org/pdf/2306.17459.pdf" type="application/pdf" width="100%" height="100%">
+                </div>
+                <div class="pdf-bottom">
+                    <el-button type="primary" @click="showPDF = false" plain size="default">Close</el-button>
+                </div>
+            </div>
+
         </el-drawer>
 
         <!-- That's the end of the list -->
         <el-empty v-show="!filteredList.length"></el-empty>
-        <el-divider v-show="filteredList.length">That's all.</el-divider>
+        <el-divider v-show="filteredList.length"><span style="color: #0088A5;">That's all.</span></el-divider>
     </div>
 
     <!-- empty pic -->
@@ -74,6 +85,7 @@
 <script setup>
 import { ref, computed } from 'vue';
 import { Search } from '@element-plus/icons-vue';
+// import axios from 'axios';
 
 const item_list = ref([
     { PO: 101320077, SKU: 164549, Quantity: 18900.000, Status: 'REL' },
@@ -86,11 +98,11 @@ const item_list = ref([
 ]);
 
 let select = ref('1');
-let query = ref('')
-let showPDF = ref(false)
+let query = ref('');
+let showPDF = ref(false);
 
 function viewPDF() {
-    showPDF.value = true
+    showPDF.value = true;
 }
 
 const filteredList = computed(() => {
@@ -102,6 +114,20 @@ const filteredList = computed(() => {
 </script>
 
 <style scoped>
+@font-face {
+    font-family: "brushup";
+    src: local("brushup"), url("~@/assets/fonts/Brushup/Brush Up.woff") format("woff");
+}
+
+@font-face {
+    font-family: "bariol";
+    src: local("bariol"), url("~@/assets/fonts/Bariol/bariol_regular-webfont.woff") format("woff");
+}
+
+html * {
+    font-family: bariol;
+}
+
 .header {
     display: flex;
     flex-direction: row;
@@ -111,12 +137,18 @@ const filteredList = computed(() => {
 }
 
 .header img {
-    height: 80px;
-    padding-top: 20px;
+    height: 100px;
+    padding-top: 5px;
+}
+
+.page-header {
+    font-size: 30px;
+    font-family: brushup;
+    color: #005EB8;
 }
 
 .desc-item {
-    box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
+    box-shadow: var(--el-box-shadow-lighter);
     margin-top: 15px;
 }
 
@@ -124,11 +156,25 @@ const filteredList = computed(() => {
     background-color: #fff;
 }
 
-.page-header {
-    font-size: 30px;
+.pdf-bottom-container {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+}
+
+
+.pdf-bottom {
+    display: flex;
+    flex-direction: row-reverse;
+    align-items: center;
+    padding: 10px;
 }
 
 :deep(.el-drawer__body) {
     padding: 0;
+}
+
+:deep(.el-descriptions__label.el-descriptions__cell.is-bordered-label) {
+    color: #002677;
 }
 </style>
